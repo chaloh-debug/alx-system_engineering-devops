48b218a4f930aac0f7ae7c4b3f07fc46f0438f54
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-23 00:12:05 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     user = '{}users/{}'.format(url, argv[1])
     req = requests.get(user)
     data = req.json()
-    print("Employee {} is done with tasks".format(data.get('name')), end="")
 
+    e_name = data.get('name')
     todos = '{}todos?userId={}'.format(url, argv[1])
     req = requests.get(todos)
     data = req.json()
@@ -23,6 +23,11 @@ if __name__ == "__main__":
         if task.get('completed') is True:
             tasks.append(task)
 
-    print("({}/{}):".format(len(tasks), len(data)))
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+                                                   e_name,
+                                                   len(tasks),
+                                                   len(data)
+                                                   ))
     for task in tasks:
         print("\t{}".format(task.get("title")))

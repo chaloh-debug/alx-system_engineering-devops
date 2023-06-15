859a4175f2112c54e0249a0bc9106d3ec201fc95
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-06-15 14:05:48 +0300

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # fix too many requests in nginx server
 
 exec {'ulimit':
+  provider => shell,
   command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
-  provider => shell
 }
 
 exec {'restart':
   provider => shell,
-  command => 'sudo service nginx restart'
+  command => 'sudo service nginx restart',
 }
